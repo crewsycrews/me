ba99cd266b8ede96749f455ceedc9d3283317c22
CrewsyCrews <danre1@bk.ru>
2018-10-26 15:33:20 +0300

    complete animating features

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,23 +7,84 @@ import {  trigger, state, style, animate, transition, keyframes} from '@angular/
   animations: [
     trigger('Blinking', [
       // ...
-      state('lightOn', style({
-        textShadow: "0px 0px 25px rgba(40, 155, 40, 0.9)"
+      state('lightGreen', style({
+        textShadow: "0px 0px 25px rgba(40, 210, 40, 0.9)"
       })),
-      state('lightOff', style({
-        textShadow: "0px 0px 25px rgba(40, 155, 40, 0.9)"
+      state('lightRed', style({
+        textShadow: "0px 0px 25px rgba(210, 40, 40, 0.9)"
       })),
-      transition('* <=> lightOn', [
-        animate('1.8s ease-out', keyframes([
-          style({ textShadow: "0px 0px 30px rgba(40, 155, 40, 0.1)", offset: 0.05}),
-          style({ textShadow: "0px 0px 25px rgba(40, 155, 40, 0.9)", offset: 0.25}),
-          style({ textShadow: "0px 0px 2px rgba(40, 155, 40, 0.1)", offset: 0.3}),
-          style({ textShadow: "0px 0px 25px rgba(40, 155, 40, 0.9)", offset: 0.35}),
-          style({ textShadow: "0px 0px 2px rgba(40, 155, 40, 0.1)", offset: 0.5}),
-          style({ textShadow: "0px 0px 25px rgba(40, 155, 40, 0.9)", offset: 0.55}),
-          style({ textShadow: "0px 0px 2px rgba(40, 155, 40, 0.1)", offset: 0.75}),
-          style({ textShadow: "0px 0px 25px rgba(40, 155, 40, 0.8)", offset: 0.95}),
-          style({ textShadow: "0px 0px 25px rgba(40, 155, 40, 0.9)", offset: 1})
+      state('lightYellow', style({
+        textShadow: "0px 0px 25px rgba(210, 210, 40, 0.9)"
+      })),
+      state('lightBlue', style({
+        textShadow: "0px 0px 25px rgba(40, 40, 210, 0.9)"
+      })),
+      state('lightWhite', style({
+        textShadow: "0px 0px 25px rgba(200, 200, 200, 0.9)"
+      })),
+      transition('* => lightGreen', [
+        animate('1.8s', keyframes([
+          style({ textShadow: "0px 0px 25px rgba(40, 210, 40, 0.1)", offset: 0.05}),
+          style({ textShadow: "0px 0px 25px rgba(40, 210, 40, 0.9)", offset: 0.25}),
+          style({ textShadow: "0px 0px 2px rgba(40, 210, 40, 0.1)", offset: 0.3}),
+          style({ textShadow: "0px 0px 25px rgba(40, 210, 40, 0.9)", offset: 0.35}),
+          style({ textShadow: "0px 0px 2px rgba(40, 210, 40, 0.1)", offset: 0.5}),
+          style({ textShadow: "0px 0px 25px rgba(40, 210, 40, 0.9)", offset: 0.55}),
+          style({ textShadow: "0px 0px 2px rgba(40, 210, 40, 0.1)", offset: 0.75}),
+          style({ textShadow: "0px 0px 25px rgba(40, 210, 40, 0.8)", offset: 0.95}),
+          style({ textShadow: "0px 0px 25px rgba(40, 210, 40, 0.9)", offset: 1})
+        ])),
+      ]),
+      transition('* => lightRed', [
+        animate('1.8s', keyframes([
+          style({ textShadow: "0px 0px 25px rgba(210, 40, 40, 0.1)", offset: 0.05}),
+          style({ textShadow: "0px 0px 25px rgba(210, 40, 40, 0.9)", offset: 0.25}),
+          style({ textShadow: "0px 0px 2px rgba(210, 40, 40, 0.1)", offset: 0.3}),
+          style({ textShadow: "0px 0px 25px rgba(210, 40, 40, 0.9)", offset: 0.35}),
+          style({ textShadow: "0px 0px 2px rgba(210, 40, 40, 0.1)", offset: 0.5}),
+          style({ textShadow: "0px 0px 25px rgba(210, 40, 40, 0.9)", offset: 0.55}),
+          style({ textShadow: "0px 0px 2px rgba(210, 40, 40, 0.1)", offset: 0.75}),
+          style({ textShadow: "0px 0px 25px rgba(210, 40, 40, 0.8)", offset: 0.95}),
+          style({ textShadow: "0px 0px 25px rgba(210, 40, 40, 0.9)", offset: 1})
+        ])),
+      ]),
+      transition('* => lightYellow', [
+        animate('1.8s', keyframes([
+          style({ textShadow: "0px 0px 25px rgba(200, 200, 40, 0.1)", offset: 0.05}),
+          style({ textShadow: "0px 0px 25px rgba(200, 200, 40, 0.9)", offset: 0.25}),
+          style({ textShadow: "0px 0px 2px rgba(200, 200, 40, 0.1)", offset: 0.3}),
+          style({ textShadow: "0px 0px 25px rgba(200, 200, 40, 0.9)", offset: 0.35}),
+          style({ textShadow: "0px 0px 2px rgba(200, 200, 40, 0.1)", offset: 0.5}),
+          style({ textShadow: "0px 0px 25px rgba(200, 200, 40, 0.9)", offset: 0.55}),
+          style({ textShadow: "0px 0px 2px rgba(200, 200, 40, 0.1)", offset: 0.75}),
+          style({ textShadow: "0px 0px 25px rgba(200, 200, 40, 0.8)", offset: 0.95}),
+          style({ textShadow: "0px 0px 25px rgba(200, 200, 40, 0.9)", offset: 1})
+        ])),
+      ]),
+      transition('* => lightBlue', [
+        animate('1.8s', keyframes([
+          style({ textShadow: "0px 0px 25px rgba(40, 40, 210, 0.1)", offset: 0.05}),
+          style({ textShadow: "0px 0px 25px rgba(40, 40, 210, 0.9)", offset: 0.25}),
+          style({ textShadow: "0px 0px 2px rgba(40, 40, 210, 0.1)", offset: 0.3}),
+          style({ textShadow: "0px 0px 25px rgba(40, 40, 210, 0.9)", offset: 0.35}),
+          style({ textShadow: "0px 0px 2px rgba(40, 40, 210, 0.1)", offset: 0.5}),
+          style({ textShadow: "0px 0px 25px rgba(40, 40, 210, 0.9)", offset: 0.55}),
+          style({ textShadow: "0px 0px 2px rgba(40, 40, 210, 0.1)", offset: 0.75}),
+          style({ textShadow: "0px 0px 25px rgba(40, 40, 210, 0.8)", offset: 0.95}),
+          style({ textShadow: "0px 0px 25px rgba(40, 40, 210, 0.9)", offset: 1})
+        ])),
+      ]),
+      transition('* => lightWhite', [
+        animate('1.8s', keyframes([
+          style({ textShadow: "0px 0px 25px rgba(200, 200, 200, 0.1)", offset: 0.05}),
+          style({ textShadow: "0px 0px 25px rgba(200, 200, 200, 0.9)", offset: 0.25}),
+          style({ textShadow: "0px 0px 2px rgba(200, 200, 200, 0.1)", offset: 0.3}),
+          style({ textShadow: "0px 0px 25px rgba(200, 200, 200, 0.9)", offset: 0.35}),
+          style({ textShadow: "0px 0px 2px rgba(200, 200, 200, 0.1)", offset: 0.5}),
+          style({ textShadow: "0px 0px 25px rgba(200, 200, 200, 0.9)", offset: 0.55}),
+          style({ textShadow: "0px 0px 2px rgba(200, 200, 200, 0.1)", offset: 0.75}),
+          style({ textShadow: "0px 0px 25px rgba(200, 200, 200, 0.8)", offset: 0.95}),
+          style({ textShadow: "0px 0px 25px rgba(200, 200, 200, 0.9)", offset: 1})
         ])),
       ]),
     ]),
@@ -32,13 +93,46 @@ import {  trigger, state, style, animate, transition, keyframes} from '@angular/
 export class AppComponent {
   title = 'Madbroz';
   height = document.documentElement.clientHeight;
-  isLighten = true;
-  toggle() {
-    this.isLighten = !this.isLighten;
+  classForText = 'textBlock0';
+  textyText = 'PHP developer';
+  textyTexts = ["PHP developer","GameDev Romantic","ZSH lover"];
+  lightColor = "lightGreen";
+  colors = ["Green", "White", "Red", "Blue", "Yellow"]
+  i=1;
+  j=1;
+  k=0;
+  colorLogoChange() {
+    this.lightColor = "light"+this.colors[this.j];
+    this.j++;
+    
+    if(this.j==5) {
+      this.j = 0;
+    }
+  };
+  colorShChange() {
+    if(this.k==4) {
+      this.k=0;
+      return;
+    }
+    this.k++;
   };
   constructor() {
   setInterval(() => { 
-    this.toggle() 
+    this.colorLogoChange() 
     },7000);
+  setTimeout(() => { 
+      this.colorShChange()
+      setInterval(() => { 
+        this.colorShChange() 
+      },7000);
+    },8800);
+  setInterval(() => { 
+    this.classForText = "textBlock" + this.i;
+    this.textyText = this.textyTexts[this.i];
+    this.i++;
+    if (this.i == 3) {
+      this.i = 0;
+    }
+    },4000);
   }
 }
