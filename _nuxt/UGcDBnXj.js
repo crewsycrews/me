52cf98005b031fa678780a08db1403b60f14b2d2
crewsycrews <danre1@bk.ru>
2026-07-29 12:54:58 +0000

    Deploying to gh-pages from @ crewsycrews/me@f7cc3f8755fa6f84feb99d9975b84d4a2244df8e 🚀

---
+++ _nuxt/UGcDBnXj.js
@@ -0,0 +1,1 @@
+import{L as e,V as t,f as n,q as r}from"./MdxqhxqD.js";import{t as i}from"./BV7S_Wam.js";var a={__name:`ProseA`,props:{href:{type:String,default:``},target:{type:String,default:void 0,required:!1}},setup(a){let o=a;return(a,s)=>{let c=i;return e(),n(c,{href:o.href,target:o.target},{default:r(()=>[t(a.$slots,`default`)]),_:3},8,[`href`,`target`])}}};export{a as default};
